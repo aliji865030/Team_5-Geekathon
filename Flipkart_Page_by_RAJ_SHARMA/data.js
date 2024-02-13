1e14e49ab7c1204aa57e0ef6a59e6a3306c95f2a
aliji865030 <82137372+aliji865030@users.noreply.github.com>
2024-02-13 20:28:33 +0530

    files added

---
+++ Flipkart_Page_by_RAJ_SHARMA/data.js
@@ -0,0 +1,466 @@
+const products = [
+
+    {
+    
+    image: "https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+    
+    title: "Product 1",
+    
+    rating: 4.5,
+    
+    flipkartAssured: true,
+    
+    price: 499.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 449.99,
+    
+    discountPrice: 50.00,
+    
+    popularity: 200,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+    
+    title: "Product 2",
+    
+    rating: 4.2,
+    
+    flipkartAssured: false,
+    
+    price: 299.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 279.99,
+    
+    discountPrice: 20.00,
+    popularity: 100,
+    
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1609942571926-f3fe26feab26?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTI2fHxwcm9kdWN0fGVufDB8fDB8fHww",
+    
+    title: "Product 3",
+    
+    rating: 3.9,
+    
+    flipkartAssured: true,
+    
+    price: 699.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 599.99,
+    
+    discountPrice: 100.00,
+    popularity: 500,
+    
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1546868871-7041f2a55e12?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 4",
+    
+    rating: 4.7,
+    
+    flipkartAssured: true,
+    
+    price: 199.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 179.99,
+    
+    discountPrice: 20.00,
+    popularity: 900,
+    
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1583947215259-38e31be8751f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 5",
+    
+    rating: 4.3,
+    
+    flipkartAssured: true,
+    
+    price: 899.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 849.99,
+    
+    discountPrice: 50.00,
+    popularity: 10,
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1609900179119-3ed9f4530eb8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTMwfHxwcm9kdWN0fGVufDB8fDB8fHww",
+    
+    title: "Product 6",
+    
+    rating: 4.0,
+    
+    flipkartAssured: false,
+    
+    price: 399.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 359.99,
+    
+    discountPrice: 40.00,
+    
+    popularity: 50,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1491637639811-60e2756cc1c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 7",
+    
+    rating: 4.8,
+    
+    flipkartAssured: true,
+    
+    price: 599.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 549.99,
+    
+    discountPrice: 50.00,
+    popularity: 1000,
+    
+    },
+    
+    {
+    
+    image: "https://plus.unsplash.com/premium_photo-1664392147011-2a720f214e01?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 8",
+    
+    rating: 4.1,
+    
+    flipkartAssured: true,
+    
+    price: 349.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: false,
+    
+    specialPrice: 329.99,
+    
+    discountPrice: 20.00,
+    popularity: 2,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1556228578-0d85b1a4d571?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 9",
+    
+    rating: 3.8,
+    
+    flipkartAssured: true,
+    
+    price: 799.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 749.99,
+    
+    discountPrice: 50.00,
+    popularity: 34634,
+    
+    },
+    
+    {
+    
+    image: "https://plus.unsplash.com/premium_photo-1679913792906-13ccc5c84d44?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzd8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 10",
+    
+    rating: 4.6,
+    
+    flipkartAssured: false,
+    
+    price: 249.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 229.99,
+    
+    discountPrice: 20.00,
+    popularity: 9800,
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1570831739435-6601aa3fa4fb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzZ8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 11",
+    
+    rating: 4.4,
+    
+    flipkartAssured: true,
+    
+    price: 599.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 579.99,
+    
+    discountPrice: 20.00,
+    popularity: 201,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1545127398-14699f92334b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 12",
+    
+    rating: 4.9,
+    
+    flipkartAssured: true,
+    
+    price: 179.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 169.99,
+    
+    discountPrice: 10.00,
+    popularity: 199,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1547949003-9792a18a2601?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTJ8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 13",
+    
+    rating: 4.2,
+    
+    flipkartAssured: true,
+    
+    price: 899.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 849.99,
+    
+    discountPrice: 50.00,
+    popularity: 2300,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1513116476489-7635e79feb27?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 14",
+    
+    rating: 4.3,
+    
+    flipkartAssured: false,
+    
+    price: 449.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 429.99,
+    
+    discountPrice: 20.00,
+    popularity: 990,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1549482199-bc1ca6f58502?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTh8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 15",
+    
+    rating: 4.7,
+    
+    flipkartAssured: true,
+    
+    price: 299.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 269.99,
+    
+    discountPrice: 30.00,
+    popularity: 1000,
+    
+    
+    },
+    
+    {
+    
+    image: "https://plus.unsplash.com/premium_photo-1670984222499-b566bf5cef69?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Njl8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 16",
+    
+    rating: 4.0,
+    
+    flipkartAssured: true,
+    
+    price: 699.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 649.99,
+    
+    discountPrice: 50.00,
+    popularity: 0,
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1619760078865-ee0f4c6586ee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NzF8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 17",
+    
+    rating: 3.9,
+    
+    flipkartAssured: true,
+    
+    price: 549.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 499.99,
+    
+    discountPrice: 50.00,
+    popularity: 240,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1608528577891-eb055944f2e7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzh8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 18",
+    
+    rating: 4.5,
+    
+    flipkartAssured: false,
+    
+    price: 499.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: true,
+    
+    specialPrice: 469.99,
+    
+    discountPrice: 30.00,
+    popularity: 560,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1585565804112-f201f68c48b4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nzl8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+    
+    title: "Product 19",
+    
+    rating: 4.6,
+    
+    flipkartAssured: true,
+    
+    price: 349.99,
+    
+    deliveryIn1Day: true,
+    
+    noCostEMI: false,
+    
+    specialPrice: 329.99,
+    
+    discountPrice: 20.00,
+    popularity: 299,
+    
+    },
+    
+    {
+    
+    image: "https://images.unsplash.com/photo-1520931061294-db3e762a9273?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTE0fHxwcm9kdWN0fGVufDB8fDB8fHww",
+    
+    title: "Product 20",
+    
+    rating: 3.8,
+    
+    flipkartAssured: true,
+    
+    price: 649.99,
+    
+    deliveryIn1Day: false,
+    
+    noCostEMI: true,
+    
+    specialPrice: 599.99,
+    
+    discountPrice: 50.00,
+    popularity: 200,
+    
+    },
+    
+    ];
